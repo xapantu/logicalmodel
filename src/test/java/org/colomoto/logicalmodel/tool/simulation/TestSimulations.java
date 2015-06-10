ec28c820466efc500e4659f2e4dfc11c60d7f034
Lucas Baudin <lucas.baudin@ens.fr>
2015-06-10 14:24:38 +0200

    Add tests for SingleSuccessorSimulation

---
+++ src/test/java/org/colomoto/logicalmodel/tool/simulation/TestSimulations.java
@@ -0,0 +1,95 @@
+package org.colomoto.logicalmodel.tool.simulation;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
+
+
+import org.colomoto.logicalmodel.LogicalModel;
+import org.colomoto.logicalmodel.LogicalModelImpl;
+import org.colomoto.logicalmodel.NodeInfo;
+import org.colomoto.logicalmodel.tool.simulation.updater.AsynchronousUpdater;
+import org.colomoto.logicalmodel.tool.simulation.updater.BlockSequentialUpdater;
+import org.colomoto.logicalmodel.tool.simulation.updater.PriorityClasses;
+import org.colomoto.logicalmodel.tool.simulation.updater.PriorityUpdater;
+import org.colomoto.logicalmodel.tool.simulation.updater.SequentialUpdater;
+import org.colomoto.logicalmodel.tool.simulation.updater.SynchronousUpdater;
+import org.colomoto.mddlib.MDDManager;
+import org.colomoto.mddlib.MDDVariable;
+import org.colomoto.mddlib.internal.MDDStoreImpl;
+import org.colomoto.mddlib.operators.MDDBaseOperators;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestSimulations {
+
+	private LogicalModel getModel() {
+		// build a list of variables and functions for a model
+		List<NodeInfo> vars = new ArrayList<NodeInfo>();
+		vars.add(new NodeInfo("A"));
+		vars.add(new NodeInfo("B"));
+		vars.add(new NodeInfo("C"));
+		
+		MDDManager manager = new MDDStoreImpl(vars, 2);
+		int[] functions = new int[vars.size()];
+		functions[0] = 1;
+		functions[1] = 1;
+		MDDVariable va = manager.getVariableForKey(vars.get(0));
+		MDDVariable vb = manager.getVariableForKey(vars.get(1));
+		int fa = va.getNode(0, 1);
+		int fb = vb.getNode(0, 1);
+		functions[2] = MDDBaseOperators.AND.combine(manager, fa, fb);
+		
+		return new LogicalModelImpl(vars, manager, functions);
+	}
+	
+	private LogicalModel getOtherModel() {
+		// build a list of variables and functions for a model
+		List<NodeInfo> vars = new ArrayList<NodeInfo>();
+		vars.add(new NodeInfo("A"));
+		vars.add(new NodeInfo("B"));
+		vars.add(new NodeInfo("C"));
+		vars.add(new NodeInfo("D"));
+		vars.add(new NodeInfo("E"));
+		
+		MDDManager manager = new MDDStoreImpl(vars, 2);
+		int[] functions = new int[vars.size()];
+		MDDVariable va = manager.getVariableForKey(vars.get(0));
+		MDDVariable vb = manager.getVariableForKey(vars.get(1));
+		MDDVariable vc = manager.getVariableForKey(vars.get(2));
+		MDDVariable vd = manager.getVariableForKey(vars.get(3));
+		MDDVariable ve = manager.getVariableForKey(vars.get(4));
+		int fa = va.getNode(0, 1);
+		int fna = va.getNode(1, 0);
+		int fb = vb.getNode(0, 1);
+		int fnb = vb.getNode(1,0);
+		int fc = vc.getNode(0, 1);
+		int fd = vd.getNode(0, 1);
+
+		functions[0] = fa;
+		functions[1] = MDDBaseOperators.AND.combine(manager, fa, fc);
+		functions[2] = fnb;
+		functions[3] = fna;
+		functions[4] = MDDBaseOperators.OR.combine(manager, fb, fd);
+		
+		return new LogicalModelImpl(vars, manager, functions);
+	}
+	
+	@Test
+	public void testSingleSuccessorSimulation() throws IOException {
+		LogicalModel model = getModel();
+        DeterministicUpdater updater = new SynchronousUpdater(model);
+		byte[] state = {0, 0, 0};
+		byte[] state2 = {1, 1, 0};
+		byte[] state3 = {1, 1, 1};
+        SingleSuccessorSimulation simulation = new SingleSuccessorSimulation(updater, state, 100);
+		
+        Iterator<byte[]> it = simulation.iterator();
+        
+		Assert.assertArrayEquals(state, it.next());
+		Assert.assertArrayEquals(state2, it.next());
+		Assert.assertArrayEquals(state3, it.next());
+
+	}
+}
